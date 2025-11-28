dbf7a4bf898bbe7440e466c4b49602216cefe148
Shagun Goyal <shagungoyal@Shagun-MacBook-Air.local>
2025-11-28 20:08:25 +0530

    trying new things

--- client/pages/Hero.jsx
+++ client/pages/Hero.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
+import axios from 'axios';
 
-function Hero() {
-  const [data, setData] = useState("Loading...");
-
-  useEffect(() => {
-    const fetchMsg = async () => {
+const fetchMsg = async () => {
       try {
         const base = "http://localhost:5100";
         const res = await fetch(base + "/api/message");
@@ -15,15 +12,50 @@ function Hero() {
           setData("Failed to load message: " + err.message);
       }
     };
-    fetchMsg();
+
+    const abc = async () => {
+    try {
+    const response = await fetch("http://localhost:5100/api/targets");
+    const data = await response.json();
+    console.log(data.message);
+    }
+    catch (err) {
+          console.error("Fetch error:", err);
+          setData("Failed to load message: " + err.message);
+      }
+    };
+
+function Hero() {
+  const [data, setData] = useState("Loading...");
+
+  useEffect(() => {
+    abc();
   }, []);
 
-  const images = {};
+  const images = [
+    {
+      id: 1,
+      url: "https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg"
+    },
+    {
+      id: 2,
+      url: "https://images.pexels.com/photos/136405/pexels-photo-136405.jpeg"
+    },
+    {id: 3,
+      url: "https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg"
+    },
+    {id: 4,
+      url: "https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg"
+    },
+    {id: 5,
+      url: "https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg"
+    }
+  ];
    
   return (
     <>
-    <div>
-        <img src = "" alt = "FTINESS IMAGES" ClassName = "h-{50%} w-screen "/>
+    <div className = "w-auto h- auto flex justify-center">
+        <img src = {images[2].url} alt = "FTINESS IMAGES" className = "h-150"/>
     </div>
     </>
   );
--- server/script.js
+++ server/script.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 5100;
+const PORT = 5100;
 
 app.use(cors());
 app.use(express.json());
